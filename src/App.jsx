d2a2a884f134f9d1745f46e7aa8a385b6140b72a
ManognaRam006 <manognaram5@gmail.com>
2025-04-10 21:59:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css'
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./pages/ProtectedRoute"
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route 
+        path="/dashboard" 
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } 
+      />
     </Routes>
   )
 }
